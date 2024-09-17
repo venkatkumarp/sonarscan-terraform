b8100fad68f288b2dc13195ae0366aa203285e0f
venkatkumarp <venkat.linux2015@gmail.com>
2024-09-17 14:53:59 +0530

    Update main.tf

--- aws/main.tf
+++ aws/main.tf
@@ -8,13 +8,3 @@ resource "aws_s3_bucket" "this" {
   tags = merge({ "resourcename" = "${local.name}" }, local.tags)
 }
 
-# Terraform backend configuration
-terraform {
-  backend "s3" {
-    bucket         = "mydata-venkey"                    # S3 bucket name where the state file is stored
-    key            = "terraform/state.tfstate"         # Path in the S3 bucket for the state file
-    region         = "us-east-1"                       # AWS region where the bucket is located
-    encrypt        = true                              # Encrypt the state file in S3
-    #dynamodb_table = "terraform-lock-table"            # DynamoDB table for state locking (optional)
-  }
-}
